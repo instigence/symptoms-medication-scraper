c52cb77f895e134b17905fb3ba0bef8d1889dcc6
neerajbansal <neeraj.bansal@canopy.cloud>
2020-01-18 16:16:21 +0800

    Successfully first iteration with results 1050/6710

--- convert_data_into_csv.py
+++ convert_data_into_csv.py
@@ -1,8 +1,12 @@
 import json
 import pandas as pd
 
+previous_data = pd.read_excel('conditions_urls_with_symptoms.xlsx')
+new_data = pd.DataFrame()
 
 with open('data.json') as json_file:
     data = json.load(json_file)
+    new_data = pd.DataFrame(data)
 
-pd.DataFrame(data).to_excel('conditions_with_urls.xlsx')
+previous_data = previous_data.append(new_data)
+pd.DataFrame(previous_data).to_excel('conditions_urls_with_symptoms.xlsx', index=False)
---
+++ spiders/condition_symptoms_spider.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import scrapy
+import requests
+
+base_url = 'https://www.drugs.com'
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'alphabets'
+    symptoms = pd.read_excel('conditions_with_urls.xlsx').to_dict(orient='records')
+    all_urls = set([base_url + symptom['condition_url'] for symptom in symptoms])
+    # Urls already fetched successfully
+    previous_urls = set(pd.read_excel('conditions_urls_with_symptoms.xlsx')['condition_url'])
+
+    start_urls = all_urls.difference(previous_urls)
+    print('Urls left to fetch %s' % len(start_urls))
+
+    def parse(self, response):
+        print('Fetching url %s' % response.url)
+        try:
+            symptoms = response.css('.contentBox h2:contains("signs and symptoms")~ul')[0] \
+                .get() \
+                .replace("<ul>", "") \
+                .replace("<li>", "") \
+                .replace("</ul>", "") \
+                .replace("</li>", "") \
+                .split("\n")
+            if symptoms is not None:
+                # symptoms["symptoms"] = list(filter(lambda sym: sym != '', symptoms))
+                print(list(filter(lambda sym: sym != '', symptoms)))
+                yield {
+                    'condition': symptoms,
+                    'condition_url': response.url
+                }
+        except Exception as e:
+            print('Error Exist in parser, may be need to change parser rules.')
+
